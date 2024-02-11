642a61b277b3f43e5880ec8ea0fed4a06ee3bfc0
mortenlove <115878787+mortenlove@users.noreply.github.com>
2024-02-11 17:09:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ app = Flask(__name__)
 # Обработка GET-запроса
 @app.route('/', methods=['GET'])
 def handle_get():
-    print('принял гет')
     return 'Hello, World! (GET)'
 
 # Обработка POST-запроса
-@app.route('/', methods=['POST'])
+@app.route('/receive-post', methods=['POST'])
 def handle_post():
-    print('принял post')
     data = request.form if request.form else request.json
     return f'Hello, World! (POST)\nReceived data: {data}'
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3000)
+    app.run(debug=True)
